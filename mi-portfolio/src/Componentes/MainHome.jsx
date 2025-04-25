b22bb64c079d614162711e6d56cc528f10812248
Maaic12 <miguelmedinakpo@gmail.com>
2025-04-25 03:20:46 -0300

    Añadida en el MainHome una card con datos pasados como props desde Main

--- mi-portfolio/src/Componentes/MainHome.jsx
+++ mi-portfolio/src/Componentes/MainHome.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
 
-const MainHome = () => {
+const MainHome = ({nombre,apodo,edad,pais,ciudad,email,lenguajes}) => {
   return (
     <div>
-      s
+      <Card style={{ width: '18rem', textAlign: 'left' }}>
+      <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+      <Card.Body>
+        <Card.Title>{nombre}</Card.Title>
+        <Card.Text>
+          {apodo} <br />
+          {edad} <br />
+          {pais} <br />
+          {ciudad} <br />
+          {email} <br />
+          {lenguajes.map((lenguaje, index) => (
+            <li key={index}>{lenguaje}</li>
+          ))}
+        </Card.Text>
+      </Card.Body>
+    </Card>
     </div>
   )
 }
--- mi-portfolio/src/Pages/Home.jsx
+++ mi-portfolio/src/Pages/Home.jsx
@@ -1,16 +1,26 @@
-import React from 'react'
-import Header from '../Componentes/Header'
-import MainHome from '../Componentes/MainHome'
-import Footer from '../Componentes/Footer'
+import React from "react";
+import Header from "../Componentes/Header";
+import MainHome from "../Componentes/MainHome";
+import Footer from "../Componentes/Footer";
 
 const Home = () => {
+  let sad = {
+    nombre: "Miguel Medina",
+    apodo: "Maic",
+    edad: 21,
+    pais: "Argentina",
+    ciudad: "Tucumán",
+    email: "miguelmedinakpo@gmail.com",
+    lenguajes: ["JavaScript", "Python", "HTML", "CSS", "React"],
+  };
+
   return (
     <div>
-      <Header/>
-      <MainHome/>
-      <Footer/>
+      <Header />
+      <MainHome {...sad} />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
